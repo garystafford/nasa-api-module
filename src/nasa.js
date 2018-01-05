3a877e26a119cf6e14dda7de274d453ee06ba7c8
Gary Stafford <gary.stafford@accenture.com>
2018-01-05 15:57:39 -0500

    Initial commit of project

---
+++ src/nasa.js
@@ -0,0 +1,52 @@
+'use strict'
+
+const rp = require('request-promise')
+
+module.exports = {
+  // Common method to make requests
+  makeRequest: function (options, callback) {
+    rp(options)
+      .then(function (body) {
+        console.log(body)
+        callback(null, body)
+      })
+      .catch(function (err) {
+        console.log('err: ' + err)
+        callback(err)
+      })
+  },
+
+  // Astronomy Picture of the Day API (APOD)
+  // https://api.nasa.gov/api.html#apod
+  getAstronomyPicOfDay: function (callback) {
+    let options = {
+      uri: 'https://api.nasa.gov/planetary/apod',
+      qs: {
+        'api_key': `${process.env.DEMO_KEY}`
+      },
+      headers: {
+        'User-Agent': 'Request-Promise'
+      },
+      json: true
+    }
+
+    this.makeRequest(options, callback)
+  },
+
+  // DSCOVR's Earth Polychromatic Imaging Camera (EPIC)
+  // https://api.nasa.gov/api.html#neows-swagger
+  getEpic: function (callback) {
+    let options = {
+      uri: 'https://api.nasa.gov/EPIC/api/natural/images',
+      qs: {
+        'api_key': `${process.env.DEMO_KEY}`
+      },
+      headers: {
+        'User-Agent': 'Request-Promise'
+      },
+      json: true
+    }
+
+    this.makeRequest(options, callback)
+  }
+}
