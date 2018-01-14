88ad7bfc0cbd277a7485d8bc75f12cd09ea06455
Gary Stafford <gary.stafford@accenture.com>
2018-01-14 09:56:31 -0500

    Updating release version

--- index.js
+++ index.js
@@ -1,19 +1,12 @@
 'use strict';
 
-const co = require('co');
 const rp = require('request-promise');
 const configClient = require('./lib/cacheClient');
 const config = configClient.loadConfigs(['DEMO_KEY', 'DEMO_KEY_ENCRYPTED']);
 
-let DEMO_KEY_ENCRYPTED = '';
-
-co(function *(){
-    DEMO_KEY_ENCRYPTED = yield Promise.resolve(config.DEMO_KEY_ENCRYPTED);
-}).catch(onerror);
-
-function onerror(err) {
-    console.error(err.stack);
-}
+let p = new Promise((resolve, reject) => {
+    return resolve(config.DEMO_KEY_ENCRYPTED)
+});
 
 module.exports = {
     // Common method to make requests
@@ -32,34 +25,37 @@ module.exports = {
     // Astronomy Picture of the Day API (APOD)
     // https://api.nasa.gov/api.html#apod
     getAstronomyPicOfDay: function (callback) {
-        let options = {
-            uri: 'https://api.nasa.gov/planetary/apod',
-            qs: {
-                'api_key': DEMO_KEY_ENCRYPTED
-            },
-            headers: {
-                'User-Agent': 'Request-Promise'
-            },
-            json: true
-        };
-        console.log(`config.DEMO_KEY: ${config.DEMO_KEY}`);
-        this.makeRequest(options, callback)
+        p.then(function (data) {
+            let options = {
+                uri: 'https://api.nasa.gov/planetary/apod',
+                qs: {
+                    'api_key': data
+                },
+                headers: {
+                    'User-Agent': 'Request-Promise'
+                },
+                json: true
+            };
+            this.makeRequest(options, callback)
+        })
     },
 
     // DSCOVR's Earth Polychromatic Imaging Camera (EPIC)
     // https://api.nasa.gov/api.html#neows-swagger
     getEpic: function (callback) {
-        let options = {
-            uri: 'https://api.nasa.gov/EPIC/api/natural/images',
-            qs: {
-                'api_key': DEMO_KEY_ENCRYPTED
-            },
-            headers: {
-                'User-Agent': 'Request-Promise'
-            },
-            json: true
-        };
-        console.log(`config.DEMO_KEY: ${config.DEMO_KEY}`);
-        this.makeRequest(options, callback)
+        p.then(function (data) {
+            let options = {
+                uri: 'https://api.nasa.gov/EPIC/api/natural/images',
+                qs: {
+                    'api_key': data
+                },
+                headers: {
+                    'User-Agent': 'Request-Promise'
+                },
+                json: true
+            };
+            this.makeRequest(options, callback)
+        })
+
     }
 };
