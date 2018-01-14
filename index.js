004c934235ef5fabbed0cf0f774056775f0a0680
Gary Stafford <gary.stafford@accenture.com>
2018-01-14 09:00:18 -0500

    Updating release version

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 'use strict';
 
+const co = require('co');
 const rp = require('request-promise');
 const configClient = require('./lib/cacheClient');
-const config = configClient.loadConfigs(['DEMO_KEY']);
+const config = configClient.loadConfigs(['DEMO_KEY', 'DEMO_KEY_ENCRYPTED']);
+
+let DEMO_KEY_ENCRYPTED = '';
+
+co(function *(){
+    DEMO_KEY_ENCRYPTED = yield Promise.resolve(config.DEMO_KEY_ENCRYPTED);
+}).catch(onerror);
+
+function onerror(err) {
+    console.error(err.stack);
+}
 
 module.exports = {
     // Common method to make requests
@@ -24,7 +35,7 @@ module.exports = {
         let options = {
             uri: 'https://api.nasa.gov/planetary/apod',
             qs: {
-                'api_key': config.DEMO_KEY
+                'api_key': DEMO_KEY_ENCRYPTED
             },
             headers: {
                 'User-Agent': 'Request-Promise'
@@ -41,7 +52,7 @@ module.exports = {
         let options = {
             uri: 'https://api.nasa.gov/EPIC/api/natural/images',
             qs: {
-                'api_key': config.DEMO_KEY
+                'api_key': DEMO_KEY_ENCRYPTED
             },
             headers: {
                 'User-Agent': 'Request-Promise'
