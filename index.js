071e259cf02a17785a9a80cb98644ae742852f88
Gary Stafford <gary.stafford@accenture.com>
2018-01-14 12:12:35 -0500

    Updating release version

--- index.js
+++ index.js
@@ -4,58 +4,62 @@ const rp = require('request-promise');
 const configClient = require('./lib/cacheClient');
 const config = configClient.loadConfigs(['DEMO_KEY', 'DEMO_KEY_ENCRYPTED']);
 
-let p = new Promise((resolve, reject) => {
-    return resolve(config.DEMO_KEY_ENCRYPTED)
+let p = new Promise((resolve) => {
+    resolve(config.DEMO_KEY_ENCRYPTED)
 });
+
+function makeRequest(options, callback) {
+    rp(options)
+        .then(function (body) {
+            console.log(body);
+            callback(null, body)
+        })
+        .catch(function (err) {
+            console.log('err: ' + err);
+            callback(err)
+        })
+}
 
 module.exports = {
     // Common method to make requests
-    makeRequest: function (options, callback) {
-        rp(options)
-            .then(function (body) {
-                console.log(body);
-                callback(null, body)
-            })
-            .catch(function (err) {
-                console.log('err: ' + err);
-                callback(err)
-            })
-    },
 
     // Astronomy Picture of the Day API (APOD)
     // https://api.nasa.gov/api.html#apod
     getAstronomyPicOfDay: function (callback) {
-        p.then(function (data) {
+        new Promise((resolve) => {
+            resolve(config.DEMO_KEY_ENCRYPTED)
+        }).then((value) => {
             let options = {
                 uri: 'https://api.nasa.gov/planetary/apod',
                 qs: {
-                    'api_key': data
+                    'api_key': value
                 },
                 headers: {
                     'User-Agent': 'Request-Promise'
                 },
                 json: true
             };
-            this.makeRequest(options, callback)
+            makeRequest(options, callback)
         })
     },
 
     // DSCOVR's Earth Polychromatic Imaging Camera (EPIC)
     // https://api.nasa.gov/api.html#neows-swagger
     getEpic: function (callback) {
-        p.then(function (data) {
+        new Promise((resolve) => {
+            resolve(config.DEMO_KEY_ENCRYPTED)
+        }).then((value) => {
             let options = {
                 uri: 'https://api.nasa.gov/EPIC/api/natural/images',
                 qs: {
-                    'api_key': data
+                    'api_key': value
                 },
                 headers: {
                     'User-Agent': 'Request-Promise'
                 },
                 json: true
             };
-            this.makeRequest(options, callback)
+            makeRequest(options, callback)
         })
-
     }
 };
