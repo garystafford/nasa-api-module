732ffd8431ea19b91bb144fbcccee199c89f8ab8
Gary Stafford <gary.stafford@accenture.com>
2018-01-13 19:00:37 -0500

    Switching to AWS Param Store

--- index.js
+++ index.js
@@ -1,52 +1,54 @@
-'use strict'
+'use strict';
 
-const rp = require('request-promise')
+const rp = require('request-promise');
+const configClient = require('./lib/cacheClient');
+const config = configClient.loadConfigs(['DEMO_KEY'], 30000);
 
 module.exports = {
-  // Common method to make requests
-  makeRequest: function (options, callback) {
-    rp(options)
-      .then(function (body) {
-        console.log(body)
-        callback(null, body)
-      })
-      .catch(function (err) {
-        console.log('err: ' + err)
-        callback(err)
-      })
-  },
+    // Common method to make requests
+    makeRequest: function (options, callback) {
+        rp(options)
+            .then(function (body) {
+                console.log(body);
+                callback(null, body)
+            })
+            .catch(function (err) {
+                console.log('err: ' + err);
+                callback(err)
+            })
+    },
 
-  // Astronomy Picture of the Day API (APOD)
-  // https://api.nasa.gov/api.html#apod
-  getAstronomyPicOfDay: function (callback) {
-    let options = {
-      uri: 'https://api.nasa.gov/planetary/apod',
-      qs: {
-        'api_key': `${process.env.DEMO_KEY}`
-      },
-      headers: {
-        'User-Agent': 'Request-Promise'
-      },
-      json: true
+    // Astronomy Picture of the Day API (APOD)
+    // https://api.nasa.gov/api.html#apod
+    getAstronomyPicOfDay: function (callback) {
+        let options = {
+            uri: 'https://api.nasa.gov/planetary/apod',
+            qs: {
+                'api_key': config.DEMO_KEY || `${process.env.DEMO_KEY}`
+            },
+            headers: {
+                'User-Agent': 'Request-Promise'
+            },
+            json: true
+        };
+
+        this.makeRequest(options, callback)
+    },
+
+    // DSCOVR's Earth Polychromatic Imaging Camera (EPIC)
+    // https://api.nasa.gov/api.html#neows-swagger
+    getEpic: function (callback) {
+        let options = {
+            uri: 'https://api.nasa.gov/EPIC/api/natural/images',
+            qs: {
+                'api_key': config.DEMO_KEY || `${process.env.DEMO_KEY}`
+            },
+            headers: {
+                'User-Agent': 'Request-Promise'
+            },
+            json: true
+        };
+
+        this.makeRequest(options, callback)
     }
-
-    this.makeRequest(options, callback)
-  },
-
-  // DSCOVR's Earth Polychromatic Imaging Camera (EPIC)
-  // https://api.nasa.gov/api.html#neows-swagger
-  getEpic: function (callback) {
-    let options = {
-      uri: 'https://api.nasa.gov/EPIC/api/natural/images',
-      qs: {
-        'api_key': `${process.env.DEMO_KEY}`
-      },
-      headers: {
-        'User-Agent': 'Request-Promise'
-      },
-      json: true
-    }
-
-    this.makeRequest(options, callback)
-  }
-}
+};
